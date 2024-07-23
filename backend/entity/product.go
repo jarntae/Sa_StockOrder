9d448d6f6ebab1d9b9db1089ec62929d89e673da
jarntae <poramet2022@gmail.com>
2024-07-23 16:44:57 +0700

    Create product.go

---
+++ backend/entity/product.go
@@ -0,0 +1,26 @@
+package entity
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Product struct {
+
+	gorm.Model
+
+	StockID           string    `json:"stock_id"`
+
+	StockName         string    `json:"stock_name"`
+
+	CategoryID        string    `json:"id_category"`
+
+	Category          *Category `gorm:"foreignKey:id_category" json:"category_id"`
+
+	LastUpdated       time.Time `json:"last_updated"`
+
+	EmployeeUpdatedID uint       `json:"employee_updated_id"`
+
+	EmployeeUpdated   *Employee  `gorm:"foreignKey:employee_updated_id" json:"employee_id"`
+}
