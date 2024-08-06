996cb9f361a413d14a63cbd0ce337230401bf0ba
jarntae <poramet2022@gmail.com>
2024-08-06 20:30:38 +0700

    Create main.go

---
+++ backend/main.go
@@ -0,0 +1,33 @@
+package main
+import (
+	"gorm.io/gorm"
+
+	"gorm.io/driver/sqlite"
+
+	"github.com/Jarntae/Sa_StockOrder/entity"
+  )
+
+
+func main() {
+	db, err := gorm.Open(sqlite.Open("Stock.db"), &gorm.Config{})
+  if err != nil {
+    panic("failed to connect database")
+  }
+  db.AutoMigrate(
+
+	&entity.Category{},
+
+	&entity.Employee{},
+
+	&entity.Product{},
+
+	&entity.Stock{},
+
+	&entity.Supplier{},
+
+	&entity.Gender{},
+
+	&entity.Position{},
+)
+	
+}
