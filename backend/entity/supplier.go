fa4a079f0d336197c5f8a8d88bf8b325ed362818
jarntae <poramet2022@gmail.com>
2024-08-06 20:30:36 +0700

    Update supplier.go

--- backend/entity/supplier.go
+++ backend/entity/supplier.go
@@ -1,18 +1,29 @@
 package entity
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Supplier struct {
+	SupplierID uint `gorm:"primaryKey" json:"supplier_id"`
 
-	gorm.Model
-
-	SupplierID   int    `json:"supplier_id"`
-	
 	SupplierName string `json:"supplier_name"`
 
-	Phone        string `json:"phone"`
+	Phone string `json:"phone"`
 
-	Email        string `json:"email"`
-	
-	Address      string `json:"address"`
+	Email string `json:"email"`
+
+	Address string `json:"address"`
+
+	Stock []Stock `gorm:"foreignKey:supplier_id"`
+
+	// เรากำหนด primaryKey  type เองเลยไม่ได้ใช้ gorm.model
+
+	CreatedAt time.Time
+
+	UpdatedAt time.Time
+
+	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
