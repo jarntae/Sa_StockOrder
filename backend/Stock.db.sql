12b848efde156e3a34b0253f0a2ada110de9eb29
jarntae <poramet2022@gmail.com>
2024-08-06 22:43:37 +0700

    Update Stock.db.sql

--- backend/Stock.db.sql
+++ backend/Stock.db.sql
@@ -1,83 +1,83 @@
 BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS "categories" (
 	"category_id"	text,
-	"category_name"	text,
 	"created_at"	datetime,
 	"updated_at"	datetime,
 	"deleted_at"	datetime,
+	"category_name"	text,
 	PRIMARY KEY("category_id")
 );
 CREATE TABLE IF NOT EXISTS "genders" (
-	"gender_id"	text,
-	"gender_name"	text,
+	"id"	integer,
 	"created_at"	datetime,
 	"updated_at"	datetime,
 	"deleted_at"	datetime,
-	PRIMARY KEY("gender_id")
+	"gender_name"	text,
+	PRIMARY KEY("id" AUTOINCREMENT)
 );
 CREATE TABLE IF NOT EXISTS "positions" (
-	"position_id"	text,
-	"position_name"	text,
+	"id"	integer,
 	"created_at"	datetime,
 	"updated_at"	datetime,
 	"deleted_at"	datetime,
-	PRIMARY KEY("position_id")
+	"position_name"	text,
+	PRIMARY KEY("id" AUTOINCREMENT)
 );
 CREATE TABLE IF NOT EXISTS "employees" (
-	"employee_id"	text,
+	"id"	integer,
+	"created_at"	datetime,
+	"updated_at"	datetime,
+	"deleted_at"	datetime,
 	"first_name"	text,
 	"last_name"	text,
 	"username"	text,
 	"password"	text,
 	"register_date"	datetime,
-	"gender_id"	text,
-	"position_id"	text,
-	"created_at"	datetime,
-	"updated_at"	datetime,
-	"deleted_at"	datetime,
-	PRIMARY KEY("employee_id"),
-	CONSTRAINT "fk_positions_employee" FOREIGN KEY("position_id") REFERENCES "positions"("position_id"),
-	CONSTRAINT "fk_genders_employee" FOREIGN KEY("gender_id") REFERENCES "genders"("gender_id")
+	"gender_id"	integer,
+	"position_id"	integer,
+	CONSTRAINT "fk_positions_employee" FOREIGN KEY("position_id") REFERENCES "positions"("id"),
+	CONSTRAINT "fk_genders_employee" FOREIGN KEY("gender_id") REFERENCES "genders"("id"),
+	PRIMARY KEY("id" AUTOINCREMENT)
 );
 CREATE TABLE IF NOT EXISTS "products" (
 	"product_id"	text,
-	"product_name"	text,
-	"category_id"	text,
-	"employee_id"	text,
 	"created_at"	datetime,
 	"updated_at"	datetime,
 	"deleted_at"	datetime,
-	PRIMARY KEY("product_id"),
+	"product_name"	text,
+	"category_id"	text,
+	"employee_id"	integer,
+	CONSTRAINT "fk_employees_product" FOREIGN KEY("employee_id") REFERENCES "employees"("id"),
 	CONSTRAINT "fk_categories_product" FOREIGN KEY("category_id") REFERENCES "categories"("category_id"),
-	CONSTRAINT "fk_employees_product" FOREIGN KEY("employee_id") REFERENCES "employees"("employee_id")
+	PRIMARY KEY("product_id")
 );
 CREATE TABLE IF NOT EXISTS "suppliers" (
-	"supplier_id"	integer,
+	"id"	integer,
+	"created_at"	datetime,
+	"updated_at"	datetime,
+	"deleted_at"	datetime,
 	"supplier_name"	text,
 	"phone"	text,
 	"email"	text,
 	"address"	text,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "stocks" (
+	"id"	integer,
 	"created_at"	datetime,
 	"updated_at"	datetime,
 	"deleted_at"	datetime,
-	PRIMARY KEY("supplier_id" AUTOINCREMENT)
-);
-CREATE TABLE IF NOT EXISTS "stocks" (
-	"stock_id"	integer,
 	"quantity"	integer,
 	"price"	real,
 	"date_in"	datetime,
 	"expiration_date"	datetime,
 	"product_id"	text,
-	"employee_id"	text,
+	"employee_id"	integer,
 	"supplier_id"	integer,
-	"created_at"	datetime,
-	"updated_at"	datetime,
-	"deleted_at"	datetime,
-	PRIMARY KEY("stock_id" AUTOINCREMENT),
 	CONSTRAINT "fk_products_stock" FOREIGN KEY("product_id") REFERENCES "products"("product_id"),
-	CONSTRAINT "fk_employees_stock" FOREIGN KEY("employee_id") REFERENCES "employees"("employee_id"),
-	CONSTRAINT "fk_suppliers_stock" FOREIGN KEY("supplier_id") REFERENCES "suppliers"("supplier_id")
+	CONSTRAINT "fk_employees_stock" FOREIGN KEY("employee_id") REFERENCES "employees"("id"),
+	CONSTRAINT "fk_suppliers_stock" FOREIGN KEY("supplier_id") REFERENCES "suppliers"("id"),
+	PRIMARY KEY("id" AUTOINCREMENT)
 );
 CREATE INDEX IF NOT EXISTS "idx_categories_deleted_at" ON "categories" (
 	"deleted_at"
