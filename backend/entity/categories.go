29f18919692940c211f401d555570432ccf1a812
jarntae <poramet2022@gmail.com>
2024-08-06 22:43:54 +0700

    Update categories.go

--- backend/entity/categories.go
+++ backend/entity/categories.go
@@ -10,12 +10,6 @@ type Category struct {
 
 	CategoryID   string `gorm:"primaryKey"  json:"category_id"`
 
-    CategoryName string `json:"category_name"`
-
-	//Category 1 to 0..* Product
-
-	Product []Product  `gorm:"foreignKey:category_id"`
-
 	// เรากำหนด primaryKey  type เองเลยไม่ได้ใช้ gorm.model
 
 	CreatedAt time.Time
@@ -23,4 +17,12 @@ type Category struct {
     UpdatedAt time.Time
 
     DeletedAt gorm.DeletedAt `gorm:"index"`
+
+    CategoryName string `json:"category_name"`
+
+	//Category 1 to 0..* Product
+
+	Product []Product  `gorm:"foreignKey:category_id"`
+
+	
 }
